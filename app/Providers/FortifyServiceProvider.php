508445898cde56131309855d59accdfb12b91947
Kang Mulyana <moelyhana@gmail.com>
2023-09-25 09:56:20 +0700

    feature(add): Login & Register

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -46,5 +46,10 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::loginView(function () {
             return view('pages.auth-login');
         });
+
+        Fortify::registerView(function() {
+            return view('pages.auth-register');
+        });
+
     }
 }
--- resources/views/pages/auth-login.blade.php
+++ resources/views/pages/auth-login.blade.php
@@ -19,18 +19,18 @@
                 action="#"
                 class="needs-validation"
                 novalidate="">
+                @csrf
                 <div class="form-group">
                     <label for="email">Email</label>
                     <input id="email"
                         type="email"
-                        class="form-control"
+                        class="form-control @error('email') is-invalid @enderror"
                         name="email"
                         tabindex="1"
-                        required
                         autofocus>
-                    <div class="invalid-feedback">
-                        Please fill in your email
-                    </div>
+                        @error('email')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
                 </div>
 
                 <div class="form-group">
@@ -46,13 +46,13 @@
                     </div>
                     <input id="password"
                         type="password"
-                        class="form-control"
+                        class="form-control @error('email') is-invalid @enderror"
                         name="password"
                         tabindex="2"
                         required>
-                    <div class="invalid-feedback">
-                        please fill in your password
-                    </div>
+                        @error('password')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
                 </div>
 
                 <div class="form-group">
@@ -75,6 +75,7 @@
                     </button>
                 </div>
             </form>
+            <!--
             <div class="mt-4 mb-3 text-center">
                 <div class="text-job text-muted">Login With Social</div>
             </div>
@@ -90,11 +91,11 @@
                     </a>
                 </div>
             </div>
-
+            -->
         </div>
     </div>
     <div class="text-muted mt-5 text-center">
-        Don't have an account? <a href="auth-register.html">Create One</a>
+        Don't have an account? <a href="{{ route('register') }}">Create One</a>
     </div>
 @endsection
 
